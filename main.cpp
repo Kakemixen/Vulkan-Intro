8f9d4572b5687a4b9a7d40b01a2169cd873582f4
Erling Ljunggren <erlingljunggren@gmail.com>
2021-07-28 18:54:46 +0200

    handling resizing!

--- main.cpp
+++ main.cpp
@@ -152,9 +152,10 @@ private:
     {
         glfwInit();
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+        glfwSetWindowUserPointer(window, this);
         glfwSetFramebufferSizeCallback(window, framebufferResizeCallback);
     }
 
