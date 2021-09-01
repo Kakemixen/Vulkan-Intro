aed540b532504c61f74bf50ea1f2d4fc1fdd9d0c
Erling Ljunggren <erlingljunggren@gmail.com>
2021-09-02 00:22:56 +0200

    refactor: vertex fix

--- vertex.cpp
+++ vertex.cpp
@@ -46,13 +46,3 @@ bool Vertex::operator==(const Vertex& other) const
 {
     return pos == other.pos && color == other.color && texCoord == other.texCoord;
 }
-
-namespace std {
-    template<> struct hash<Vertex> {
-        size_t operator()(Vertex const& vertex) const {
-            return ((hash<glm::vec3>()(vertex.pos) ^
-                   (hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^
-                   (hash<glm::vec2>()(vertex.texCoord) << 1);
-        }
-    };
-}
